b8f5c296540b90cf3068c688f1f83b0040b6181e
Kesav2k04 <kesavk659@gmail.com>
2025-05-30 20:13:22 +0530

    Remove all old files from repository

--- backend/src/main/java/com/example/demo/repository/ExclusivesProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.ExclusivesProduct;
-
-public interface ExclusivesProductRepository extends JpaRepository<ExclusivesProduct, Long> {
-}
--- backend/src/main/java/com/example/demo/repository/WomenProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.WomenProduct;
-
-public interface WomenProductRepository extends JpaRepository<WomenProduct, Long> {
-}
