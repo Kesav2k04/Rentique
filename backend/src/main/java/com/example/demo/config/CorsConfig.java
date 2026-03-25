6c6bdedc32a3e70fadd486d85ed426cb40191e59
Kesav Kumar J <115524444+Kesav2k04@users.noreply.github.com>
2026-03-25 11:35:50 +0530

    Create CorsConfig.java

---
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return registry -> registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
